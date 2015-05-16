17925a67e6ae39f6efeda8acbcf9c3210aefe022
Alejandra Cordero <alejandra.corderogarcia21@gmail.com>
2015-05-16 13:46:32 -0400

    reduce.c esta casi limpio. Falta pulir algunos comentarios y limpiar estructuras creadas

--- Proyecto1/reduce.c
+++ Proyecto1/reduce.c
@@ -31,6 +31,169 @@ typedef struct lista {
 } LISTA;
 
 
+
+//----------------------------------------------------------------------------//
+
+void EliminarEstructuraLista(LISTA **Cabecera){
+
+	/*
+
+		Definicion de la funcion:	
+			Esta funcion destruye una estructura del tipo LISTA.
+	
+		Parametros de entrada:
+
+			- Cabecera : Direccion de memoria de una caberera de una estructuta 
+						del tipo LISTA.
+	
+	 	Parametros de salida:
+	 		Ninguno.
+	
+	*/
+
+	// Declaracion de variables:
+	LISTA *aux;
+	aux = *Cabecera;
+
+
+	if (aux==NULL){
+		;
+	}
+	else{
+
+		while(aux!=NULL){
+
+			*Cabecera= aux-> siguiente;
+			free(aux->elem);
+			free(aux);
+			aux = *Cabecera;
+
+		}
+
+	}
+
+}
+//----------------------------------------------------------------------------//
+void ReduceProcesos(char *Persona1,char *Persona2,char *Amigos,FILE *archivoSalida){
+
+	/*
+
+		Definicion de la funcion:	
+
+			Esta funcion, dado el nombre de dos personas y una lista de amigos 
+		realiza el algortimo de reduce y escribe el resultado en un archivo de
+		salida que es pasado por los parametros de la funcion.
+	
+		Parametros de entrada:
+
+			- Persona1 : Nombre de una persona.
+			- Persona2 : Nombre de una persona.
+			- Amigos : Lista de amigos de las variables Persona1 y Persona2.
+			- archivoSalida : Archivo donde se escribira el resultado de reduce.
+
+	 	Parametros de salida:
+	 		Ninguno.
+	
+	*/
+
+	// Declaracion de variables:
+	int amigosEnComun = 0;
+	char *token;
+    LISTA *listaAmigos1=NULL;
+	LISTA *aux1;
+	LISTA *nueva_caja1;
+
+	LISTA *listaAmigos2=NULL;
+	LISTA *aux2;
+	LISTA *nueva_caja2;
+
+    fprintf(archivoSalida,"(%s %s) -> ",Persona1,Persona2);
+
+   
+
+ 	// Se almacenan los amigos en listas enlazadas:
+
+					
+	// Se crea el primer nodo:
+	token = strtok(Amigos," ");
+	nueva_caja1 = (LISTA*)malloc(sizeof(LISTA));
+	nueva_caja1->elem = token;
+	listaAmigos1 = nueva_caja1;
+	aux1 = nueva_caja1;
+					
+
+	while( strcmp(token,",") != 0)  {
+
+		token = strtok(NULL," ");
+		if (strcmp(token,",") != 0) {
+
+			nueva_caja1 = (LISTA*)malloc(sizeof(LISTA));
+			nueva_caja1->elem = token;
+			aux1->siguiente = nueva_caja1;
+			aux1 = nueva_caja1;
+
+		}
+
+	}
+			
+	while(token != NULL) {
+
+		if(listaAmigos2==NULL){
+
+			nueva_caja2 = (LISTA*)malloc(sizeof(LISTA));
+			token = strtok(NULL," ");
+			nueva_caja2->elem = token;
+			listaAmigos2 = nueva_caja2;
+			aux2 = nueva_caja2;
+
+		}
+
+		else{
+			token = strtok(NULL," ");
+			if (token != NULL) {
+				nueva_caja2 = (LISTA*)malloc(sizeof(LISTA));
+				nueva_caja2->elem = token;
+				aux2->siguiente = nueva_caja2;
+				aux2 = nueva_caja2;
+
+				}
+			}
+
+	}
+						
+	aux1 = listaAmigos1;
+
+	while (aux1 != NULL) {
+
+		aux2 = listaAmigos2;
+
+		while(aux2 != NULL) {
+
+			if (strcmp(aux1->elem,aux2->elem) == 0) {
+				amigosEnComun = 1;
+				fprintf(archivoSalida,"%s ",aux1->elem);
+				break;
+
+			}
+
+			aux2 = aux2-> siguiente;
+		}
+
+		aux1 = aux1->siguiente;
+
+	}
+
+	if (amigosEnComun == 0) {
+		fprintf(archivoSalida,"-None-");
+	}
+	
+	fprintf(archivoSalida,"\n");
+
+}
+
+//----------------------------------------------------------------------------//
+
+
 int main(int argc, char *argv[]) {
 
 	/*
@@ -61,14 +224,9 @@ int main(int argc, char *argv[]) {
 	// Se convierte el PID en string
 	sprintf(nombreSalida, "%d",miPID);
 	strcat(nombreSalida, ".txt");
-	printf("reduce %s\n",argv[0]);
-
-
-
 
 	if ( fopen(argv[0],"r") == NULL ) {
 
-		printf("No tengo trabajo asignado\n");
 		exit(1);
 	}
 
@@ -84,7 +242,6 @@ int main(int argc, char *argv[]) {
    		if (ftell(archivoEntrada) == 0 )	{
    			
    			//El archivo esta vacio.
-   			printf("No tengo trabajo asignado\n");
 			fclose(archivoEntrada);	
 			remove(argv[0]);
    			exit(0);
@@ -100,21 +257,12 @@ int main(int argc, char *argv[]) {
     			realizaReduce = fgetc(archivoEntrada);
     			realizaReduce = realizaReduce - 48;
 
-				printf("Realiza Reduce: %d\n",realizaReduce);
-
     			if (realizaReduce == 0) {
-    				printf("No tengo amigos en comun\n");
+
     				Persona1 = (char*)malloc(sizeof(char)*15);
     				Persona2 = (char*)malloc(sizeof(char)*15);
 
-
     				fscanf(archivoEntrada," (%[^ (] %[^ )] ) \n" ,Persona1,Persona2);
-
-    				//printf("////////////////////////////\n");
-    				//printf("Persona 1: %s.\n",Persona1);
-					//printf("Persona 2: %s.\n",Persona2);
-					//printf("////////////////////////////\n");
-
 					fprintf(archivoSalida,"(%s %s) -> -None-\n",Persona1,Persona2);
 
 					while (fgetc(archivoEntrada) != 10);
@@ -124,140 +272,16 @@ int main(int argc, char *argv[]) {
 
     			else if (realizaReduce == 1) {
 
-    				printf("TENGA AMIGOS EN COMUN\n");
 					Persona1 = (char*)malloc(sizeof(char)*15);
     				Persona2 = (char*)malloc(sizeof(char)*15);
 					Amigos = (char*)malloc(sizeof(char)*100);
 
     				fscanf(archivoEntrada," (%[^ (] %[^ )] ) -> %[^\n]\n",Persona1,Persona2,Amigos);
 
-    				fprintf(archivoSalida,"(%s %s) -> ",Persona1,Persona2);
-
-    				int amigosEnComun = 0;
-    				// Se almacenan los amigos en listas enlazadas:
-
-    				char *token;
-
-    				LISTA *listaAmigos1=NULL;
-					LISTA *aux1;
-					LISTA *nueva_caja1;
-
-					LISTA *listaAmigos2=NULL;
-					LISTA *aux2;
-					LISTA *nueva_caja2;
-					
-					// Se crea la primera caja:
-					token = strtok(Amigos," ");
-					nueva_caja1 = (LISTA*)malloc(sizeof(LISTA));
-					nueva_caja1->elem = token;
-					listaAmigos1 = nueva_caja1;
-					aux1 = nueva_caja1;
-					
-
-					while( strcmp(token,",") != 0)  {
-
-						//printf("SOY EL TOKEN: %s.\n",token);
-						token = strtok(NULL," ");
-						if (strcmp(token,",") != 0) {
-
-							nueva_caja1 = (LISTA*)malloc(sizeof(LISTA));
-							nueva_caja1->elem = token;
-							aux1->siguiente = nueva_caja1;
-							aux1 = nueva_caja1;
-
-						}
-
-					}
-			
-					//if (strtok_r(Amigos2," ") < 0) {
-					//	printf("ERROR DE LECTURA\n");
-					//}
-
-					while(token != NULL) {
-
-						if(listaAmigos2==NULL){
-
-							nueva_caja2 = (LISTA*)malloc(sizeof(LISTA));
-							token = strtok(NULL," ");
-							nueva_caja2->elem = token;
-							listaAmigos2 = nueva_caja2;
-							aux2 = nueva_caja2;
-
-						}
-
-						else{
-							//printf("SOY EL TOKEN 2: %s.\n",token);
-							token = strtok(NULL," ");
-							if (token != NULL) {
-								nueva_caja2 = (LISTA*)malloc(sizeof(LISTA));
-								nueva_caja2->elem = token;
-								aux2->siguiente = nueva_caja2;
-								aux2 = nueva_caja2;
-
-							}
-						}
-
-					}
-
-					aux1 = listaAmigos1;
-					aux2 = listaAmigos2;
-
-					while(aux1!=NULL){
-						printf("%s Estoy en la lista1 \n",aux1->elem);
-						aux1 = aux1->siguiente;
-					}
-
-					while(aux2!=NULL){
-						printf("%s Estoy en la lista2 \n",aux2->elem);
-						aux2 = aux2->siguiente;
-					}
-
-						
-					aux1 = listaAmigos1;
-
-					while (aux1 != NULL) {
-
-						aux2 = listaAmigos2;
-
-						while(aux2 != NULL) {
-
-							if (strcmp(aux1->elem,aux2->elem) == 0) {
-
-								amigosEnComun = 1;
-
-								fprintf(archivoSalida,"%s ",aux1->elem);
-								break;
-
-							}
-
-							aux2 = aux2-> siguiente;
-
-
-						}
-
-						aux1 = aux1->siguiente;
-
-					}
-
-					if (amigosEnComun == 0) {
-						fprintf(archivoSalida,"-None-");
-					}
-
-					
-					fprintf(archivoSalida,"\n");
-
-    				printf("---------------------------\n");
-    				printf("Persona 1: %s.\n",Persona1);
-					printf("Persona 2: %s.\n",Persona2);
-					printf("---------------------------\n");
+    				// Se aplica reduce
+    				ReduceProcesos(Persona1,Persona2,Amigos,archivoSalida);
 
     			}
-
-
-    			else {
-    				printf("entre aqui y el valor de realizaReduce es %d",realizaReduce);
-    			}
-
 
 
     			// Se verifica si se ha llegado al fin del archivo
@@ -268,37 +292,14 @@ int main(int argc, char *argv[]) {
 					fclose(archivoSalida);
 				}
 
-
-				//Persona1 = (char*)malloc(sizeof(char)*15);
-				//Amigos1 = (char*)malloc(sizeof(char)*30);
-				//Amigos2 = (char*)malloc(sizeof(char)*30);
-
-				//fscanf(archivoEntrada,"%d (%[^ (] %[^ )] ) -> %[^\n]\n" ,&realizaReduce,Persona1,Amigos1,Amigos2);
-
-				//fscanf(archivoEntrada," %[^ ->] -> %[^\n]\n" ,Persona1,Amigos1);
-				//printf("Persona 1: %s.\n",Persona1);
-				///printf("Persona 2: %s.\n",Persona2);
-				//printf("amigos 1: %s.\n",Amigos1);
-				//printf("amigos 2: %s.\n",Amigos2);
-
     		}
 
 
     	}
 
 
-
-
 	}
 
 	return(0);
-
-
-
-
-
-
-
-
 
 }
